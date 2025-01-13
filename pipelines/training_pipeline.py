4cfce2c2f20ff5b3695ce05bd47cc4672e54d634
Dhananjay Aggarwal <dhananjayaggarwal6561@gmail.com>
2025-01-13 16:58:07 +0530

    Checkpoint-16 Feature Engineering Step created and integrated with pipeline succesfully

--- pipelines/training_pipeline.py
+++ pipelines/training_pipeline.py
@@ -10,6 +10,7 @@ sys.path.append(root_dir)
 # Now you can use absolute imports
 from steps.data_ingestion_step import data_ingestion_step
 from steps.handle_missing_values_step import handle_missing_values_step
+from steps.feature_engineering_step import feature_engineering_step
 
 ## Define a pipeline
 @pipeline(
@@ -30,6 +31,11 @@ def ml_pipeline():
 
     ## Handle Missing Values
     filled_data = handle_missing_values_step(raw_data)
+
+    ##Feature Engineering
+    engineered_data = feature_engineering_step(filled_data)
+    filled_data, strategy="log", features=['SalePrice', 'Gr Liv Area']
+
 
 # Run the pipeline
 if __name__ == "__main__":
