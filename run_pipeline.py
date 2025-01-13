eefa50fe467ae84a1ae61a93ba68c31ebceb34cb
Dhananjay Aggarwal <dhananjayaggarwal6561@gmail.com>
2025-01-13 18:25:09 +0530

    created run_pipeline.py

---
+++ run_pipeline.py
@@ -0,0 +1,28 @@
+import click
+from pipelines.training_pipeline import ml_pipeline
+from zenml.integrations.mlflow.mlflow_utils import get_tracking_uri
+
+
+@click.command()
+def main():
+    """
+    Run the ML pipeline and start the MLflow UI for experiment tracking.
+    """
+    # Run the pipeline
+    run = ml_pipeline()
+
+    # Retrieve the output of the model_building_step
+    trained_model = run.steps["model_building_step"].outputs["output_name"]
+    print(f"Trained Model Type: {type(trained_model)}")
+
+    # Print MLflow UI instructions
+    print(
+        "Now run \n "
+        f"    mlflow ui --backend-store-uri '{get_tracking_uri()}'\n"
+        "To inspect your experiment runs within the mlflow UI.\n"
+        "You can find your runs tracked within the experiment."
+    )
+
+
+if __name__ == "__main__":
+    main()
