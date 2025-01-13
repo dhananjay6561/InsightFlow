402b8f0ff26a6cd090e1aceb34b45c3513d2dda8
Dhananjay Aggarwal <dhananjayaggarwal6561@gmail.com>
2025-01-13 17:09:07 +0530

    modified training_pipeline script

--- pipelines/training_pipeline.py
+++ pipelines/training_pipeline.py
@@ -7,7 +7,7 @@ from zenml import step
 root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(root_dir)
 
-# Now you can use absolute imports
+
 from steps.data_ingestion_step import data_ingestion_step
 from steps.handle_missing_values_step import handle_missing_values_step
 from steps.feature_engineering_step import feature_engineering_step
@@ -32,10 +32,12 @@ def ml_pipeline():
     ## Handle Missing Values
     filled_data = handle_missing_values_step(raw_data)
 
-    ##Feature Engineering
-    engineered_data = feature_engineering_step(filled_data)
-    filled_data, strategy="log", features=['SalePrice', 'Gr Liv Area']
+    ## Feature Engineering
+    engineered_data = feature_engineering_step(
+        filled_data, strategy="log", features=['SalePrice', 'Gr Liv Area']
+    )
 
+    # You can add more steps here, such as model training, evaluation, etc.
 
 # Run the pipeline
 if __name__ == "__main__":
