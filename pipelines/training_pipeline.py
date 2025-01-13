e099af4e292a812a39ed55019d9ff7c8c56b323c
Dhananjay Aggarwal <dhananjayaggarwal6561@gmail.com>
2025-01-13 17:53:24 +0530

    Checkpoint-17 Outlier Detection and Data Splitting integrated in pipeline succesfully

--- pipelines/training_pipeline.py
+++ pipelines/training_pipeline.py
@@ -11,6 +11,8 @@ sys.path.append(root_dir)
 from steps.data_ingestion_step import data_ingestion_step
 from steps.handle_missing_values_step import handle_missing_values_step
 from steps.feature_engineering_step import feature_engineering_step
+from steps.outlier_detection_step import outlier_detection_step
+from steps.data_splitter_step import data_splitter_step
 
 ## Define a pipeline
 @pipeline(
@@ -37,7 +39,11 @@ def ml_pipeline():
         filled_data, strategy="log", features=['SalePrice', 'Gr Liv Area']
     )
 
-    # You can add more steps here, such as model training, evaluation, etc.
+    ## Outlier Detection
+    clean_data = outlier_detection_step(engineered_data, column_name="SalePrice")
+
+    # Data Splitting Step
+    X_train, X_test, y_train, y_test = data_splitter_step(clean_data, target_column="SalePrice")
 
 # Run the pipeline
 if __name__ == "__main__":
